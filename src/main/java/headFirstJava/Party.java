16474888ca563ebdd1de90829f5faf5b4692a66e
Matias Miguez <matiaspakua@gmail.com>
2023-03-23 00:31:52 +0000

    Start reading Head First Java eth Edition

---
+++ src/main/java/headFirstJava/Party.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+import java.awt.event.*;
+
+class Party{
+	public void buildInvite(){
+		Frame f = new Frame();
+		Label l = new Label("Party at tims");
+		Button b = new Button("You bet!");
+		Button c = new Button("Shoot me");
+		Panel p = new Panel();
+		p.add(l);
+	}
+}

c0f2d5387f190d5901a880ecb349e07845032434
Matias Miguez <matiaspakua@gmail.com>
2023-04-06 21:26:37 +0000

    Fixing dataStructures

--- src/JavaDataStructures/Array.java
+++ src/JavaDataStructures/Array.java
@@ -1,5 +1,3 @@
-package java.JavaDataStructures;
-
 import java.util.Arrays;
 
 public class Array{
--- src/JavaDataStructures/ArraysMethods.java
+++ src/JavaDataStructures/ArraysMethods.java
@@ -1,16 +1,28 @@
-package main.java.JavaDataStructures;
+import java.util.Arrays;
+
 public class ArraysMethods {
 
-    public void arrayToUpperCase(String[] input) {
-      for(int i = 0; i < input.length; i++) {
-        input[i] = input[i].toUpperCase();
-      }
-    }
-  
-    public void incrementArray(int[] input) {
-      for (int i = 0; i < input.length; i++){
-        input[i] = input[i]-1;
-      }
-    }
-  
+  public static void main(String[] args) {
+
+    String[] params = new String[] { "one", "two" };
+    arrayToUpperCase(params);
+
+    int[] intParams = new int[] { 1, 2, 3 };
+    incrementArray(intParams);
   }
+
+  public static void arrayToUpperCase(String[] input) {
+    for (int i = 0; i < input.length; i++) {
+      input[i] = input[i].toUpperCase();
+    }
+    Arrays.stream(input).forEach(System.out::println);
+  }
+
+  public static void incrementArray(int[] input) {
+    for (int i = 0; i < input.length; i++) {
+      input[i] = input[i] - 1;
+    }    
+    Arrays.stream(input).forEach(System.out::println);
+  }
+
+}

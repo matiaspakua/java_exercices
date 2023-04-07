1d69ed6298ada64690cb6a8ba8e94af2c9c43b5d
Matias Miguez <matiaspakua@gmail.com>
2023-04-07 16:52:23 +0000

    Adding hacker rank exercixes.

---
+++ src/HackerRank/Day0.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Day0{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        String userInput = scan.nextLine();
+
+        scan.close();
+
+        String generalMessage = "Hello World!";
+
+        System.out.println(userInput + " " + generalMessage);
+    }
+
+}
